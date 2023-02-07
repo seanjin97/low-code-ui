340124e6b1ee59d6238cc292f4f39dcb56e0699f
SJ <seanjin97@gmail.com>
2023-02-07 08:29:51 +0000

    common api test

---
+++ pages/Configuration/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,11 @@
+export default {
+	mockConnections: () => {
+		return [
+    {"name": "VALIDATION", "state": "off", "actions": [{"action": "Create"}]},
+    {"name": "CREATE_APPLICATION", "state": "off", "actions": [{"action": "Create"}]},
+    {"name": "ADDITIONAL_API", "state": "off", "actions": [{"action": "Create"}]},
+    {"name": "CALCULATE_FEE", "state": "off", "actions": [{"action": "Create"}]},
+    {"name": "File Transfer (L1T to Agency)", "state": "off", "actions": [{"action": "Create"}]}
+		]
+	}
+}
