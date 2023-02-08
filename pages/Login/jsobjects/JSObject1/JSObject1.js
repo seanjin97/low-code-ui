be302f002086413a3f93a7c753f624392f2fed24
SJ <seanjin97@gmail.com>
2023-02-08 07:58:17 +0000

    System generated commit, after creating a new branch: test

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -2,6 +2,7 @@ export default {
 
 	Login: async () => {
 		if (Username.text && Password.text) {
+			// hello
 					const res = await LOGIN.run()
 					showAlert(`logged in successfully`)
 					storeValue("token", res.access_token)
